e3bd0f3a542b8d3342c3f38fd1d5b87ca97f050e
duongCute123 <89206817+duongCute123@users.noreply.github.com>
2022-09-22 13:19:24 +0700

    finish addcat

--- src/Components/AddCat/AddCat.jsx
+++ src/Components/AddCat/AddCat.jsx
@@ -1,24 +1,56 @@
 import React from "react";
 import { useState } from "react";
-const AddCat=()=>{
-    return(
+const AddCat = () => {
+    const list = {
+        namecat: '',
+        desscription: '',
+        gia: '',
+        anh: '',
+        country: ''
+    }
+    const country = [
+        {
+            value: "",
+            label: "----Vui lòng chọn quốc gia----"
+        },
+        {
+            label: "Việt Nam",
+            value: "Việt Nam"
+        },
+        {
+            label: "Thanh Hóa",
+            value: "Thanh Hoa"
+        },
+        {
+            label: "Sài Gòn",
+            value: "Sài Gòn"
+        },
+        {
+            label: "Hà Nội",
+            value: "Hà Nội"
+        }
+    ]
+    return (
         <div className="">
             <h1>Trang thêm sản phẩm vào nhá</h1>
-            <div class="container">
-                <form>
-                    <div class="form-group row">
-                        <label for="inputName" class="col-sm-1-12 col-form-label"></label>
-                        <div class="col-sm-1-12">
-                            <input type="text" class="form-control" name="inputName" id="inputName" placeholder=""/>
-                        </div>
-                    </div>
-                    <div class="form-group row">
-                        <div class="offset-sm-2 col-sm-10">
-                            <button type="submit" class="btn btn-primary">Action</button>
-                        </div>
-                    </div>
-                </form>
-            </div>
+            <form action="" method="post">
+                <label htmlFor="">Nhập tên của mèo</label> <br />
+                <input type="text" name="" placeholder="Nhập tên của mèo" id="" /> <br />
+                <label htmlFor="">Chọn hình ảnh của mèo</label> <br />
+                <input type="file" name="" placeholder="Chọn hình ảnh của mèo" id="" /> <br />
+                <label htmlFor="">Nhập thông tin cần thiết của mèo</label> <br />
+                <textarea placeholder="Nhập thông tin của mèo" rows={10} cols={120} /> <br />
+                <label htmlFor="">Nhập giá mèo</label> <br />
+                <input type="text" name="" id="" /> <br />
+                <label htmlFor="">Chọn quốc gia</label> <br />
+                <select name="" id="">
+                    {
+                        country.map((country) => (
+                            <option value={country.value}>{country.label}</option>
+                        ))
+                    }
+                </select>
+            </form>
         </div>
     )
 }
